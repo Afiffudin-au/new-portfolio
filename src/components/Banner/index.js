117bc452c0abf14951f0b6d54d050c2e204da84a
Afiffudin-au <afiffudin077@gmail.com>
2022-06-27 17:38:57 +0700

    update sosial profile

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Banner.scss'
-import { AiFillGithub } from 'react-icons/ai'
+import { AiFillFacebook, AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 function Banner() {
   return (
     <div className='jumbotron jumbotron-fluid'>
       <div className='container'>
-        <h1 className='title'>I'm Afiffudin-au.</h1>
+        <h1 className='title'>I'm T Afiffudin.</h1>
         <p className='desc'>
           I'm a based Indonesia. Front-End Developer Every Day Every Time.
         </p>
@@ -14,9 +14,19 @@ function Banner() {
             href='https://github.com/Afiffudin-au/'
             target='_blank'
             rel='noreferrer'>
-            <AiFillGithub
-              style={{ fontSize: '30px', color: 'white', cursor: 'pointer' }}
-            />
+            <AiFillGithub />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/tosulafiffudin/'
+            target='_blank'
+            rel='noreferrer'>
+            <AiFillLinkedin />
+          </a>
+          <a
+            href='https://www.facebook.com/afiff.udin.01110011'
+            target='_blank'
+            rel='noreferrer'>
+            <AiFillFacebook />
           </a>
         </div>
       </div>
--- src/hooks/useGetLanguageStats/index.js
+++ src/hooks/useGetLanguageStats/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { useState } from 'react'
 import { getLanguageAPI } from '../../api-calls/language'
-import formatBytes from '../../lib/formatByte'
 import { toast } from 'react-toastify'
 import { header_auth_github } from '../../config/api-config'
 let jsLanguage = []
